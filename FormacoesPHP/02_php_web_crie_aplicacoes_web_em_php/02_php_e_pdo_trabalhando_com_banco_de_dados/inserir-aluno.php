e89764590701425331d8c104eeb143c07cddfed4
LUCAS GABRIEL DA SILVA VAZ SEVERO <lucasgabriel31032006@gmail.com>
2024-09-12 19:35:02 -0400

    php puro com pdo

---
+++ FormacoesPHP/02_php_web_crie_aplicacoes_web_em_php/02_php_e_pdo_trabalhando_com_banco_de_dados/inserir-aluno.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once 'vendor/autoload.php';
+
+$databasePath = __DIR__ . '/banco.sqlite';
+$pdo = new PDO("sqlite:$databasePath");
+
+$student = new \Alura\Pdo\Domain\Model\Student(
+    null,
+    'Lucas Gabriel',
+    new \DateTimeImmutable('2006-03-31')
+);
+
+$sqlInsert = "INSERT INTO students (name, birth_date) VALUES ('{$student->name()}', '{$student->birthDate()->format('Y-m-d')}');";
+
+$pdo->exec($sqlInsert);
---
+++ FormacoesPHP/02_php_web_crie_aplicacoes_web_em_php/02_php_e_pdo_trabalhando_com_banco_de_dados/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'de831654e91d90ea9b543c6536a7fbb8bb36d47c',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'de831654e91d90ea9b543c6536a7fbb8bb36d47c',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
