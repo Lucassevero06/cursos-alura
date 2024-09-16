091f2254f63ae721c35dd70cf2c09bf970c7f3ed
lucas.severo <lucasgabriel31032006@gmail.com>
2024-09-16 12:48:50 -0400

    Update SeriesController.php

--- FormacoesPHP/03_laravel_crie_aplicacoes_web_em_php/controle-series/app/Http/Controllers/Api/SeriesController.php
+++ FormacoesPHP/03_laravel_crie_aplicacoes_web_em_php/controle-series/app/Http/Controllers/Api/SeriesController.php
@@ -5,11 +5,17 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\SeriesFormRequest;
 use App\Models\Series;
+use App\Repositories\SeriesRepository;
 use Illuminate\Http\Request;
 use function dd;
+use function response;
 
 class SeriesController extends Controller
 {
+    public function __construct(private SeriesRepository $seriesRepository)
+    {
+    }
+
     public function index()
     {
         return Series::all();
@@ -17,6 +23,32 @@ class SeriesController extends Controller
 
     public function store(SeriesFormRequest $request)
     {
-        return response()->json(Series::create($request->all()), 201);
+        return response()
+            ->json($this->seriesRepository->add($request), 201);
+    }
+
+    public function show(int $series)
+    {
+        $seriesModel = Series::with('seasons.episodes')->find($series);
+        if($seriesModel ===  null) {
+            return response()->json([
+                'message' => 'Series not found'
+            ]);
+        }
+        return $seriesModel;
+    }
+
+    public function update(Series $series, SeriesFormRequest $request)
+    {
+        $series->fill($request->all());
+        $series->save();
+
+        return $series;
+    }
+
+    public function destroy(Series $series)
+    {
+        Series::destroy($series);
+        return response()->noContent();
     }
 }
