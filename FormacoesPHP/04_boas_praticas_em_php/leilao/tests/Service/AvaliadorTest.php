6062ccbd3bd72a8daa1885df67cb7482d79d72c1
lucas.severo <lucasgabriel31032006@gmail.com>
2024-09-18 08:52:57 -0400

    estudando test php

--- FormacoesPHP/04_boas_praticas_em_php/leilao/tests/Service/AvaliadorTest.php
+++ FormacoesPHP/04_boas_praticas_em_php/leilao/tests/Service/AvaliadorTest.php
@@ -11,6 +11,11 @@ use const PHP_EOL;
 
 class AvaliadorTest extends TestCase
 {
+    private $leiloeiro;
+    protected function setUp(): void
+    {
+        $this->leiloeiro = new Avaliador();
+    }
     /**
      * @dataProvider leilaoEmOrdemAleatoria
      * @dataProvider leilaoEmOrdemCrescente
@@ -18,11 +23,9 @@ class AvaliadorTest extends TestCase
      * */
     public function testAvaliadorDeveEncontrarOMaiorValorDeLances(Leilao $leilao)
     {
-        $leiloeiro = new Avaliador();
+        $this->leiloeiro->avalia($leilao);
 
-        $leiloeiro->avalia($leilao);
-
-        $maiorValor = $leiloeiro->getMaiorValor();
+        $maiorValor = $this->leiloeiro->getMaiorValor();
 
         self::assertEquals(2500, $maiorValor);
     }
@@ -34,11 +37,9 @@ class AvaliadorTest extends TestCase
      * */
     public function testAvaliadorDeveEncontrarOMenorValorDeLances(Leilao $leilao)
     {
-        $leiloeiro = new Avaliador();
+        $this->leiloeiro->avalia($leilao);
 
-        $leiloeiro->avalia($leilao);
-
-        $menorValor = $leiloeiro->getMenorValor();
+        $menorValor = $this->leiloeiro->getMenorValor();
 
         self::assertEquals(1700, $menorValor);
     }
@@ -50,10 +51,9 @@ class AvaliadorTest extends TestCase
      * */
     public function testAvaliadorDeveBuscarOs3MaioresValores(Leilao $leilao)
     {
-        $leiloeiro = new Avaliador();
-        $leiloeiro->avalia($leilao);
+        $this->leiloeiro->avalia($leilao);
 
-        $maiores = $leiloeiro->getMaioresLances();
+        $maiores = $this->leiloeiro->getMaioresLances();
 
         static::assertCount(3, $maiores);
         static::assertEquals(2500, $maiores[0]->getValor());
@@ -61,6 +61,7 @@ class AvaliadorTest extends TestCase
         static::assertEquals(1700, $maiores[2]->getValor());
     }
 
+    /*---------- DADOS ----------*/
     public function leilaoEmOrdemCrescente()
     {
         $leilao = new Leilao('Fiat 147 0KM');
