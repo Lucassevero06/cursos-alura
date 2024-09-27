d7ed4b81df4c2d534f44a9ce07ae0723970800e0
lucas.severo <lucasgabriel31032006@gmail.com>
2024-09-27 11:35:23 -0400

    estudando react

--- FormacoesPHP/07_react_js/react_entendendo_como_a_biblioteca_funciona/ceep/src/components/FormularioCadastro/FormularioCadastro.jsx
+++ FormacoesPHP/07_react_js/react_entendendo_como_a_biblioteca_funciona/ceep/src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -2,18 +2,47 @@ import React, {Component} from "react";
 import './estilo.css';
 
 class FormularioCadastro extends Component {
+    constructor(props) {
+        super(props);
+        this.titulo = '';
+        this.texto = '';
+    }
+
+    _handleMudancaDeTitulo(evento) {
+        evento.stopPropagation();
+        this.titulo = evento.target.value;
+        console.log('Titulo: ' ,this.titulo);
+    }
+
+    _handleMudancaDeTexto(evento) {
+        evento.stopPropagation();
+        this.texto = evento.target.value;
+        console.log('Texto: ' ,this.texto);
+    }
+
+    _criarNota(evento) {
+        evento.preventDefault();
+        evento.stopPropagation();
+        this.props.criarNota(this.titulo, this.texto);
+    }
+    
     render() {
         return  (
-            <form className="form-cadastro">
-                <input 
+            <form 
+                className="form-cadastro"
+                onSubmit={this._criarNota.bind(this)}
+            >
+                <input
                     type="text" 
                     placeholder='titulo'
                     className="form-cadastro_input"
+                    onChange={this._handleMudancaDeTitulo.bind(this)}
                 />
                 <textarea 
                     rows={15}
                     placeholder='escreva sua nota'
                     className="form-cadastro_input"
+                    onChange={this._handleMudancaDeTexto.bind(this)}
                 />
 
                 <button className="form-cadastro_input form-cadastro_submit">
