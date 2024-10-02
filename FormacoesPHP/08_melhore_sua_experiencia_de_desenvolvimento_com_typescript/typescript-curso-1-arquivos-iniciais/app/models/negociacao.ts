8437acb0a6fb28ebda19e7aaa965ca0d819df0e2
LUCAS GABRIEL DA SILVA VAZ SEVERO <lucasgabriel31032006@gmail.com>
2024-10-01 20:47:26 -0400

    estudando typescript

--- FormacoesPHP/08_melhore_sua_experiencia_de_desenvolvimento_com_typescript/typescript-curso-1-arquivos-iniciais/app/models/negociacao.ts
+++ FormacoesPHP/08_melhore_sua_experiencia_de_desenvolvimento_com_typescript/typescript-curso-1-arquivos-iniciais/app/models/negociacao.ts
@@ -1,12 +1,17 @@
 export default class Negociacao {
 
     constructor(
-        public readonly data: Date, 
+        private _data: Date, 
         public readonly quantidade: number, 
         public readonly valor: number
     ) {}
     
     get volume(): number {
         return this.quantidade * this.valor;
+    }
+
+    get data(): Date {
+        const data = new Date(this._data.getTime()); //programação defensiva
+        return data;
     }
 }
