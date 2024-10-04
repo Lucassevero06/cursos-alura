fa09aac480e9b630ac9052a3b8f6b2e2f8aa7098
lucas.severo <lucasgabriel31032006@gmail.com>
2024-10-04 08:21:51 -0400

    estudando nextjs

---
+++ FormacoesPHP/09_next_js/code-connect/src/app/layout.js
@@ -0,0 +1,20 @@
+import './globals.css';
+import { Aside } from "@/components/Aside";
+
+export const metadata = {
+  title: "Code Connect",
+  description: "Uma rede social para devs!",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="pt-br">
+      <body>
+        <div className='app-container'>
+          <Aside />
+          {children}
+        </div>
+      </body>
+    </html>
+  );
+}
