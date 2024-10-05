2144e14573b23b822dfc0c575393435471f67587
LUCAS GABRIEL DA SILVA VAZ SEVERO <lucasgabriel31032006@gmail.com>
2024-10-05 17:46:42 -0400

    estudando java

--- FormacoesPHP/10_aprenda_java_orientado_a_objetos/screenmatch/src/br/com/alura/screenmatch/modelos/Titulo.java
+++ FormacoesPHP/10_aprenda_java_orientado_a_objetos/screenmatch/src/br/com/alura/screenmatch/modelos/Titulo.java
@@ -8,6 +8,11 @@ public class Titulo {
     private int totalDeAvaliacoes;
     private int duracaoEmMinutos;
 
+    public Titulo(String nome, int anoDeLancamento) {
+        this.nome = nome;
+        this.anoDeLancamento = anoDeLancamento;
+    }
+
     public String getNome() {
         return nome;
     }
@@ -57,4 +62,6 @@ public class Titulo {
     public double pegaMedia() {
         return somaDasAvaliacoes / totalDeAvaliacoes;
     }
+
+
 }
