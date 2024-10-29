f4ee8593beb2eaaa6e330a9c727803c4cab02111
lucas.severo <lucasgabriel31032006@gmail.com>
2024-10-29 14:05:40 -0400

    estudando java spring boot

---
+++ FormacoesAlura/11_java_web_crie_aplicacoes_usando_spring_boot/screenmatch/src/main/java/br/com/alura/screenmatch/controller/EpisodioDTO.java
@@ -0,0 +1,8 @@
+package br.com.alura.screenmatch.controller;
+
+public record EpisodioDTO(
+    Integer temporada,
+    Integer numeroEpisodio,
+    String titulo
+) {
+}
--- FormacoesAlura/11_java_web_crie_aplicacoes_usando_spring_boot/screenmatch/src/main/java/br/com/alura/screenmatch/controller/SerieController.java
+++ FormacoesAlura/11_java_web_crie_aplicacoes_usando_spring_boot/screenmatch/src/main/java/br/com/alura/screenmatch/controller/SerieController.java
@@ -36,4 +36,20 @@ public class SerieController
     public SerieDTO obterPorId(@PathVariable Long id) {
         return servico.obterPorId(id);
     }
+
+    @GetMapping("/{id}/temporadas/todas")
+    public List<EpisodioDTO> obterTodasTemporadas(@PathVariable Long id) {
+        return servico.obterTodasTemporadas(id);
+    }
+
+    @GetMapping("/{id}/temporadas/{numero}")
+    public List<EpisodioDTO> obterTemporadasPorNumero(@PathVariable Long id, @PathVariable Long numero) {
+        return servico.obterTemporadasPorNumero(id, numero);
+    }
+
+    @GetMapping("/categoria/{nomeGenero}")
+    public List<SerieDTO> obterSeriesPorCategoria(@PathVariable String nomeGenero) {
+        return servico.obterSeriesPorCategoria(nomeGenero);
+    }
+
 }
