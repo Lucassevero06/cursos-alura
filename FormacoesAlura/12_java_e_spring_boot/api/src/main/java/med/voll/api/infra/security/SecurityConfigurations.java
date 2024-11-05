1733a0364a3b51f45ed65ad3c963aedde7839418
lucas.severo <lucasgabriel31032006@gmail.com>
2024-11-05 13:27:52 -0400

    estudando java spring boot

--- FormacoesAlura/12_java_e_spring_boot/api/src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
+++ FormacoesAlura/12_java_e_spring_boot/api/src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
@@ -1,7 +1,9 @@
 package med.voll.api.infra.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,16 +12,26 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfigurations {
 
+    @Autowired
+    private SecurityFilter securityFilter;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http.csrf(csrf -> csrf.disable())
-                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .build();
+        return
+                http.csrf(csrf -> csrf.disable()) //desabilitando a proteção contra ataques csrf pois é apenas um api
+                        .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))//informando que a api será do formato STATELESS ou seja, não guardará sessions
+                        .authorizeHttpRequests(req -> {
+                            req.requestMatchers(HttpMethod.POST, "/login").permitAll(); //permitindo acesso a /login sem authentication
+                            req.anyRequest().authenticated()
+                                    .and().addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class); // informando que todas as outras url/rotas só ppodem ser acessadas após authenticar
+                        })
+                        .build(); //criando a configuração de segurança
     }
 
     @Bean
