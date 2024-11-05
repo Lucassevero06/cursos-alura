99d602886a5ec9474681ac44faef16c34dc98675
lucas.severo <lucasgabriel31032006@gmail.com>
2024-11-05 14:41:31 -0400

    estudando javascript

---
+++ FormacoesAlura/13_aprenda_a_programar_em_javascript_com_foco_no_back_end/js_arrays/atualizar-lista.js
@@ -0,0 +1,10 @@
+const alunos = ['João', 'Juliana', 'Caio', 'Ana'];
+const medias = [10, 8, 7.5, 9];
+
+const lista = [alunos, medias];
+
+function exibeNomeENota(aluno) {
+    if (lista[0].includes(aluno)) {
+        
+    }
+}
