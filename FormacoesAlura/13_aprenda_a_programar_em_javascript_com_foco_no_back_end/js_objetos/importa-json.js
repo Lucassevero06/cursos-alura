dbd0df464e1d399e0bfb91d309a596a269a28930
lucas.severo <lucasgabriel31032006@gmail.com>
2024-11-06 08:02:04 -0400

    estudando javascript

---
+++ FormacoesAlura/13_aprenda_a_programar_em_javascript_com_foco_no_back_end/js_objetos/importa-json.js
@@ -0,0 +1,7 @@
+const estudante = require('./estudante.json');
+
+const stringEstudante = JSON.stringify(estudante);
+console.log(stringEstudante);
+
+const objEstudante = JSON.parse(stringEstudante);
+console.log(objEstudante);
