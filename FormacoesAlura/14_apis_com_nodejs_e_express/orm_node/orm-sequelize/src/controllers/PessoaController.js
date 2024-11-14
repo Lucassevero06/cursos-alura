bc477d67128ecc7012ea6644f18b3da2c8bde73d
lucas.severo <lucasgabriel31032006@gmail.com>
2024-11-14 10:40:17 -0400

    estudando orm expressjs

--- FormacoesAlura/14_apis_com_nodejs_e_express/orm_node/orm-sequelize/src/controllers/PessoaController.js
+++ FormacoesAlura/14_apis_com_nodejs_e_express/orm_node/orm-sequelize/src/controllers/PessoaController.js
@@ -9,6 +9,17 @@ class PessoaController extends Controller {
         super(pessoaServices);
     }
 
+    async pegaMatriculas(req, res) {
+        const { estudanteId } = req.params;
+        try {
+            const listaMatriculas = await pessoaServices.pegaMatriculasPorEstudante(Number(estudanteId));
+
+            res.status(200).json(listaMatriculas);
+        } catch (erro) {
+            //erro
+        }
+    }
+
 }
 
 module.exports = PessoaController;
