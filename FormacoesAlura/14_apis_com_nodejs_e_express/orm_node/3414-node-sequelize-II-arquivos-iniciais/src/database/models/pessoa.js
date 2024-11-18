d8b134d599a17b1f814626981a873c434aec973e
lucas.severo <lucasgabriel31032006@gmail.com>
2024-11-18 10:32:27 -0400

    estudando orm express

--- FormacoesAlura/14_apis_com_nodejs_e_express/orm_node/3414-node-sequelize-II-arquivos-iniciais/src/database/models/pessoa.js
+++ FormacoesAlura/14_apis_com_nodejs_e_express/orm_node/3414-node-sequelize-II-arquivos-iniciais/src/database/models/pessoa.js
@@ -1,4 +1,6 @@
 'use strict';
+const isCpfValido = require('../../utils/validaCpfHelper.js');
+
 const {
   Model
 } = require('sequelize');
@@ -16,9 +18,32 @@ module.exports = (sequelize, DataTypes) => {
     }
   }
   Pessoa.init({
-    nome: DataTypes.STRING,
-    email: DataTypes.STRING,
-    cpf: DataTypes.STRING,
+    nome: {
+      type: DataTypes.STRING,
+      validate: {
+        len: {
+          args: [3, 50],
+          msg: 'O campo nome deve ter entre 3 e 50 caracteres'
+        }
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: {
+          args: true,
+          msg: 'E-mail inválido'
+        }
+      }
+    },
+    cpf: {
+      type: DataTypes.STRING,
+      validate: {
+        cpfEhValido: (cpf) => {
+          if (!isCpfValido(cpf)) throw new Error('Numero de CPF inválido');
+        }
+      }
+    },
     ativo: DataTypes.BOOLEAN,
     role: DataTypes.STRING
   }, {
