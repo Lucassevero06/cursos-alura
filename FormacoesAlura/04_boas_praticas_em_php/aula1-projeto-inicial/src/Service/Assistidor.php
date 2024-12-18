41c4f704fa5a452da8e37ccd25b85a138e002392
lucas.severo <lucasgabriel31032006@gmail.com>
2024-12-18 10:58:32 -0400

    entendendo o conceito SOLID

---
+++ FormacoesAlura/04_boas_praticas_em_php/aula1-projeto-inicial/src/Service/Assistidor.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Alura\Solid\Service;
+
+use Alura\Solid\Model\Assistivel;
+use Alura\Solid\Model\Pontuavel;
+
+class Assistidor
+{
+    public function assistir(Assistivel $conteudo)
+    {
+        $conteudo->assistir();
+    }
+}
